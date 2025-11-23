287c923df64ceeb5942edae4140d928924f1b549
amdevops989 <am.devops989@gmail.com>
2025-11-23 14:16:10 +0100

    prom

---
+++ prom-operator/prometheus/main.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "kube_prom_stack" {
+  name       = "kube-prom-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  create_namespace = false
+
+  values = [
+    file("${path.module}/values-kube-prom.yaml")
+  ]
+
+  depends_on = [
+    kubernetes_namespace.monitoring
+  ]
+}
---
+++ prom-operator/prometheus/namespace.tf
@@ -0,0 +1,9 @@
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+
+    labels = {
+      monitoring = "prometheus"
+    }
+  }
+}
